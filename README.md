6645f239f1b827053d3285ac833eb59c9a86de5c
Joe Contini <efisio@gmail.com>
2016-07-30 16:10:15 +0700

    Added Google Keep scraper

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # scrapers
 Scrapers I put together with lots of help from Stack Overflow.
+
+## keep-scrape.py
+Apparently there's no simple way to export your notes from Google Keep to a CSV or spreadsheet. This does that from a Google Takeout archive. Instructions:
+
+1. Go to [Google Takeout](https://takeout.google.com/settings/takeout)
+2. Make sure the 'Keep' checkbox is selected (you can deselect all others)
+3. Download and export the archive to a folder that has the 'Keep' folder
+4. Place **keep-scrape.py** in the same folder that has the 'Keep' folder
+5. Run ``python keep-scrape.py``. That should export your notes to a CSV.
---
+++ keep-scrape.py
@@ -0,0 +1,46 @@
+import os, bs4, glob, csv
+from dateutil.parser import parse
+from datetime import datetime
+
+files = glob.glob("Keep/*.html")
+notes = []
+
+#Prep CSV file
+now = datetime.now()
+csvout = "notes_%s.csv" % now.strftime("%Y-%m-%d_%H%M")
+writer = csv.writer(open(csvout, 'wb'))
+writer.writerow(['date', 'title', 'content', 'file'])
+
+for file in files:
+	page = open(file)
+	soup = bs4.BeautifulSoup(page.read())
+
+	#Make Excel-Friendly date
+	googDate = soup.select('.heading')[0].getText().strip()
+	xlDate = datetime.strftime(parse(googDate), '%m/%d/%Y %H:%M')
+
+	#Convert <br>'s to line breaks.
+	for br in soup.select('.content')[0].find_all("br"):
+		br.replace_with("\n")
+
+	try:
+		note = {
+			"date": xlDate,
+			"title": soup.select('.title')[0].getText(),
+			"content": soup.select('.content')[0].getText()
+		}
+
+		print "\n" + ('-'*15) + file + ('-'*15)
+		print 'Title: \"' + note['title'] + "\" // Date: " + note['date']
+		print ('-'*15) + '-'*int(len(file)) + ('-'*15)
+		print note['content']
+
+		writer.writerow([note['date'],note['title'],note['content'], file])
+
+	#In case a note has a blank title or content, continue anyway
+	except:
+		pass
+
+print ('\n'+('-'*20)+'\nDone! %s notes saved to CSV.\n'+('-'*20)) % len(files)
+
+#Github: https://github.com/jcontini
