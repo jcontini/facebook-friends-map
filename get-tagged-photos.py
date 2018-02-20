5d5964b745aa1e5a26160577525f1f3ee88feab5
Joe Contini <efisio@gmail.com>
2018-02-20 23:35:47 +0700

    Some improvements

--- get-tagged-photos.py
+++ get-tagged-photos.py
@@ -1,6 +1,9 @@
-import argparse, sys, time
+import argparse, sys, time, csv, wget
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def main(username, password):
     #Start Browser
@@ -14,26 +17,36 @@ def main(username, password):
     email_id.send_keys(username)
     pass_id.send_keys(password)
     driver.find_element_by_id("loginbutton").click()
-    #Nav to photos I'm tagged in
+    #Nav to photos I'm tagged in page
     driver.find_element_by_id("navItem_2305272732").click()
-    time.sleep(3)
+    time.sleep(4)
     driver.find_elements_by_css_selector(".uiMediaThumbImg")[0].click()
     #Get Photo URLs
     photo_urls = []
+    photos = []
+
     while True:
         time.sleep(1)
-        img_url = driver.find_element_by_xpath("//img[@class = 'spotlight']").get_attribute('src')
-        if img_url in photo_urls:
+        photo = {
+            'img_url': driver.find_element_by_xpath("//img[@class = 'spotlight']").get_attribute('src'),
+            'img_date': driver.find_element_by_xpath('//*[@id="fbPhotoSnowliftTimestamp"]/a[1]/abbr/span').text,
+        }
+        
+        if photo['img_url'] in photo_urls:
             break
+
         else:
-            photo_urls.append(img_url)
-            print(img_url)
-            driver.find_element_by_css_selector(".snowliftPager.next").click()
-    #Save Photo URLs to CSV
-    fd = open('photo_urls.csv','w')
-    for photo_url in photo_urls:
-        fd.write(photo_url)
-    fd.close()
+            print(photo)
+            photos.append(photo)
+            photo_urls.append(photo['img_url'])
+            driver.find_element_by_xpath('//*[@id="facebook"]/body').send_keys(u'\ue014')
+
+    #Download the photos & save an index of them to CSV
+    writer = csv.writer(open('photo_urls.csv', 'wb'))
+    for photo in photos:
+        writer.writerow([photo['img_url'],photo['img_date']])
+        dl = wget.download(photo['img_url'])
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
