60a9398ff0b531c5202e49f52c55644bc8467ebf
mwolfe <mark.wolfe@bamtechmedia.com>
2018-04-17 10:33:48 -0400

    Disable SSL verification for download

--- get-tagged-photos.py
+++ get-tagged-photos.py
@@ -1,4 +1,4 @@
-import argparse, sys, os, time, wget, json, piexif
+import argparse, sys, os, time, wget, json, piexif, ssl
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -8,6 +8,7 @@ from datetime import datetime
 from dateutil.parser import parse
 
 def download_photos():
+    ssl._create_default_https_context = ssl._create_unverified_context
     #Prep the download folder
     folder = 'photos/'
     if not os.path.exists(folder):
